d633ad0bca1ae600d5c0b87324c974a9e0a521a9
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2026-02-24 16:31:11 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const app = require('./app');
 const { sequelize } = require('./models');
 
 const PORT = process.env.PORT || 3000;
-
+console.log('Tentando conectar ao banco...');
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`🚀 API rodando na porta ${PORT}`);
+  
   });
 }).catch(err => {
   console.error('Erro ao conectar com o banco:', err);
 });
+
