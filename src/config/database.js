43278321ca509acd77206d3bdda2939d0b3d43e2
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2026-02-24 15:40:10 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,10 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false
   }
 );
+
 module.exports = sequelize;
-
-
-
