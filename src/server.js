df919718771fcd93695864dfc79008c945d531d7
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2026-02-24 15:03:49 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 const app = require('./app');
 const { sequelize } = require('./models');
 
+const PORT = process.env.PORT || 3000;
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 API rodando na porta ${PORT}`);
+  });
+});
+
+/*const app = require('./app');
+const { sequelize } = require('./models');
+
 sequelize.sync().then(() => {
   app.listen(3000, () => {
     console.log('🚀 API rodando em http://localhost:3000');
   });
 });
+
+*/
